c5d2fbe5e8ddc2726e88ccc156bbca69398c01bb
dsoto06 <diegosr06@hotmail.com>
2021-04-22 02:31:41 +0000

    Navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -3,15 +3,40 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
+		<div className="justify-content-between align-items-center border border-bottom bg-light">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<div className="text-center">
+					<h1>Pura Vida Academy</h1>
+				</div>
 			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
+
+			<div className="text-center">
+				<h4>The best place to learn</h4>
 			</div>
-		</nav>
+			<div className="container text-center">
+				<div className="row">
+					<div className="col-3">
+						<Link to="/">
+							<h3 className="text-dark">Home</h3>
+						</Link>
+					</div>
+					<div className="col-3">
+						<Link to="/demo">
+							<h3 className="text-dark">Courses</h3>
+						</Link>
+					</div>
+					<div className="col-3">
+						<Link to="/">
+							<h3 className="text-dark">Profile</h3>
+						</Link>
+					</div>
+					<div className="col-3">
+						<Link to="/demo">
+							<h3 className="text-dark">Information</h3>
+						</Link>
+					</div>
+				</div>
+			</div>
+		</div>
 	);
 };
