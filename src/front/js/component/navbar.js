64e62141b8bbad73a25049593db42bb8cf44f55a
dsoto06 <diegosr06@hotmail.com>
2021-04-22 04:36:27 +0000

    Modificaciones

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -5,7 +5,7 @@ export const Navbar = () => {
 	return (
 		<div className="justify-content-between align-items-center border border-bottom bg-light">
 			<Link to="/">
-				<div className="text-center">
+				<div className="text-center my-3">
 					<h1>Pura Vida Academy</h1>
 				</div>
 			</Link>
@@ -13,26 +13,26 @@ export const Navbar = () => {
 			<div className="text-center">
 				<h4>The best place to learn</h4>
 			</div>
-			<div className="container text-center">
+			<div className="container text-center my-3">
 				<div className="row">
 					<div className="col-3">
 						<Link to="/">
-							<h3 className="text-dark">Home</h3>
+							<h5 className="text-dark">Home</h5>
 						</Link>
 					</div>
 					<div className="col-3">
 						<Link to="/demo">
-							<h3 className="text-dark">Courses</h3>
+							<h5 className="text-dark">Courses</h5>
 						</Link>
 					</div>
 					<div className="col-3">
 						<Link to="/">
-							<h3 className="text-dark">Profile</h3>
+							<h5 className="text-dark">Profile</h5>
 						</Link>
 					</div>
 					<div className="col-3">
 						<Link to="/demo">
-							<h3 className="text-dark">Information</h3>
+							<h5 className="text-dark">Information</h5>
 						</Link>
 					</div>
 				</div>
