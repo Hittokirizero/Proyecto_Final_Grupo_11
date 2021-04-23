9583ce1ced9365d4612405beb3cd7d6c534fed4c
dsoto06 <diegosr06@hotmail.com>
2021-04-23 04:03:40 +0000

    Jumbotron

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
+	<footer className="footer text-center">
+		<div>
+			<h3>Follow us</h3>
+			<h3>
+				<i className="fab fa-facebook" /> <i className="fab fa-twitter" />
+				<i className="fab fa-youtube" />
+				<i className="fab fa-github" />
+			</h3>
+		</div>
 	</footer>
 );
