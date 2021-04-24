00977bdc0fd37e3d64e30a7813090a853bbe718c
Maria-7514 <evangeline73.mjhe@gmail.com>
2021-04-24 01:51:24 +0000

    Cards horizontales

---
+++ src/front/js/component/Cards2.js
@@ -0,0 +1,34 @@
+import React from "react"
+export const cards2 = () => {
+    <div className="first">
+    <div className="card mb-3" style="max-width: 540px;">
+        <div className="row g-0">
+            <div className="col-md-4">
+                <img src="..." alt="..."></img>
+            </div>
+        </div>
+    </div>
+    <div className="col-md-8">
+        <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+        </div>
+    </div>
+
+    <div class="card mb-3" style="max-width: 540px;">
+        <div class="row g-0">
+            <div class="col-md-4">
+                <img src="..." alt="..."></img>
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+}
