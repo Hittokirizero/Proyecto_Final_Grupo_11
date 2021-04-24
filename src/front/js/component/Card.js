d79cd76d7df5422f1a0ededdf8ca95c21c4e45eb
dsoto06 <diegosr06@hotmail.com>
2021-04-24 01:35:29 +0000

    cards

---
+++ src/front/js/component/Card.js
@@ -0,0 +1,73 @@
+import React from "react";
+import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button } from "reactstrap";
+import { Jumbotron, Container } from "reactstrap";
+import "../../styles/demo.scss";
+
+const Example = props => {
+	return (
+		<div className="my-5">
+			<div className="align-items-center text-center">
+				<h3>Special Deals and Last-Minute Offers</h3>
+				<p>
+					Book a unique tour at a great price! Grab a last-minute offer and start packing for your memorable
+					vacation.
+				</p>
+			</div>
+
+			<div className="container align-items-center">
+				<div className="row align-items-start">
+					<div className="col-4 text-center">
+						<Card>
+							<CardImg top width="100%" src="https://picsum.photos/200" alt="Card image cap" />
+							<CardBody>
+								<CardTitle tag="h5">Card title</CardTitle>
+								<CardSubtitle tag="h6" className="mb-2 text-muted">
+									Card subtitle
+								</CardSubtitle>
+								<CardText>
+									Some quick example text to build on the card title and make up the bulk of the cards
+									content.
+								</CardText>
+								<Button>Button</Button>
+							</CardBody>
+						</Card>
+					</div>
+					<div className="col-4 text-center">
+						<Card>
+							<CardImg top width="100%" src="https://picsum.photos/200" alt="Card image cap" />
+							<CardBody>
+								<CardTitle tag="h5">Card title</CardTitle>
+								<CardSubtitle tag="h6" className="mb-2 text-muted">
+									Card subtitle
+								</CardSubtitle>
+								<CardText>
+									Some quick example text to build on the card title and make up the bulk of the cards
+									content.
+								</CardText>
+								<Button>Button</Button>
+							</CardBody>
+						</Card>
+					</div>
+					<div className="col-4 text-center">
+						<Card>
+							<CardImg top width="100%" src="https://picsum.photos/200" alt="Card image cap" />
+							<CardBody>
+								<CardTitle tag="h5">Card title</CardTitle>
+								<CardSubtitle tag="h6" className="mb-2 text-muted">
+									Card subtitle
+								</CardSubtitle>
+								<CardText>
+									Some quick example text to build on the card title and make up the bulk of the cards
+									content.
+								</CardText>
+								<Button>Button</Button>
+							</CardBody>
+						</Card>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Example;
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,13 +1,15 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import Jumbotron from "../component/Jumbotron.js";
+import Card from "../component/Card.js";
 import "../../styles/home.scss";
+import "../../styles/demo.scss";
 
 export const Home = () => {
 	return (
 		<div>
 			<Jumbotron />
-			<Jumbotron />
+			<Card />
 			<Jumbotron />
 		</div>
 	);
