2cd163c33274f3e3531dab115a4d0399b904f902
dsoto06 <diegosr06@hotmail.com>
2021-04-24 21:42:50 +0000

    Dropdown

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from "reactstrap";
 import { Link } from "react-router-dom";
 
-export const Navbar = () => {
+export const Navbar = props => {
+	const [dropdownOpen, setDropdownOpen] = useState(false);
+
+	const toggle = () => setDropdownOpen(prevState => !prevState);
+
 	return (
 		<div className="justify-content-between align-items-center border border-bottom bg-light">
 			<Link to="/">
@@ -23,13 +28,23 @@ export const Navbar = () => {
 					</div>
 					<div className="col-3">
 						<Link to="/">
-							<h5 className="text-dark">Profile</h5>
+							<h5 className="text-dark">Information</h5>
 						</Link>
 					</div>
 					<div className="col-3">
-						<Link to="/demo">
-							<h5 className="text-dark">Information</h5>
-						</Link>
+						<Dropdown isOpen={dropdownOpen} toggle={toggle}>
+							<DropdownToggle tag="span" data-toggle="dropdown" aria-expanded={dropdownOpen}>
+								<h5>Profile</h5>
+							</DropdownToggle>
+							<DropdownMenu>
+								<DropdownItem header>Actions</DropdownItem>
+								<Link to="/demo">
+									<DropdownItem>My Profile</DropdownItem>
+								</Link>
+								<DropdownItem divider />
+								<DropdownItem>Log Out</DropdownItem>
+							</DropdownMenu>
+						</Dropdown>
 					</div>
 				</div>
 			</div>
