2c8b83b3e2057322fafe213ce6a5909b40b7a125
dsoto06 <diegosr06@hotmail.com>
2021-05-01 03:08:37 +0000

    courses 2

--- src/front/js/component/Card.js
+++ src/front/js/component/Card.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useContext } from "react";
+import ReactStars from "react-rating-stars-component";
+import { render } from "react-dom";
 import { Context } from "../store/appContext";
-import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button, Jumbotron, Container } from "reactstrap";
+import { Card, Button, CardImg, CardTitle, CardText, CardGroup, CardSubtitle, CardBody, Progress } from "reactstrap";
 import "../../styles/demo.scss";
 
 const Example = props => {
@@ -12,74 +14,36 @@ const Example = props => {
 
 	const category = store.courses.map((item, index) => {
 		return (
-			<div className="container align-items-center" key={index}>
-				<div className="row align-items-start">
-					<div className="col-4 text-center">
-						<Card>
-							<CardImg top width="100%" src="https://picsum.photos/1920/1080" alt="Card image cap" />
-							<CardBody>
-								<CardTitle tag="h5">{item.category}</CardTitle>
-								<CardSubtitle tag="h6" className="mb-2 text-muted">
-									{item.info_detail}
-								</CardSubtitle>
-								<CardText>
-									{item.cost}
-									{item.is_active}
-									{item.rating}
-									{item.tutor_id_fk}
-									{item.tutorships_name}
-								</CardText>
-								<Button>Button</Button>
-							</CardBody>
-						</Card>
-					</div>
-					<div className="col-4 text-center">
-						<Card>
-							<CardImg top width="100%" src="https://picsum.photos/1920/1080" alt="Card image cap" />
-							<CardBody>
-								<CardTitle tag="h5">{item.category}</CardTitle>
-								<CardSubtitle tag="h6" className="mb-2 text-muted">
-									Card subtitle
-								</CardSubtitle>
-								<CardText>
-									Some quick example text to build on the card title and make up the bulk of the cards
-									content.
-								</CardText>
-								<Button>Button</Button>
-							</CardBody>
-						</Card>
-					</div>
-					<div className="col-4 text-center">
-						<Card>
-							<CardImg top width="100%" src="https://picsum.photos/1920/1080" alt="Card image cap" />
-							<CardBody>
-								<CardTitle tag="h5">Card title</CardTitle>
-								<CardSubtitle tag="h6" className="mb-2 text-muted">
-									{" "}
-									Card subtitle
-								</CardSubtitle>
-								<CardText>
-									Some quick example text to build on the card title and make up the bulk of the cards
-									content.
-								</CardText>
-								<Button>Button</Button>
-							</CardBody>
-						</Card>
-					</div>
-				</div>
+			<div className="container shadow-lg p-3 mb-5 bg-body rounded" key={index}>
+				<CardGroup>
+					<Card>
+						<CardImg className="rounded" src={item.imagen} />
+					</Card>
+					<Card>
+						<CardBody className="bg-light text-dark ">
+							<CardTitle className="text-center" tag="h1">
+								{item.tutorships_name}
+							</CardTitle>
+							<CardSubtitle tag="h6" className="mb-2 text-muted">
+								{item.category}
+							</CardSubtitle>
+							<CardText>
+								<p>{item.info_detail}</p>
+								<p>{item.info_specifies}</p>
+
+								<ReactStars count={5} value={item.rating} size={24} activeColor="#ffd700" />
+							</CardText>
+						</CardBody>
+					</Card>
+				</CardGroup>
 			</div>
 		);
 	});
 
 	return (
-		<div className="my-5">
+		<div>
 			<div className="align-items-center text-center">
-				<h3>Special Deals and Last-Minute Offers</h3>
-				<p>
-					Book a unique tour at a great price! Grab a last-minute offer and start packing for your memorable
-					vacation.
-					{category}
-				</p>
+				<p>{category}</p>
 			</div>
 		</div>
 	);
